44a377db5ad499010e4b1c272857d145387af63d
Olga <olgahorozova75@gmail.com>
2025-04-09 21:57:03 +0300

    help

--- ShopApp.Core/Book.cs
+++ ShopApp.Core/Book.cs
@@ -14,7 +14,7 @@ namespace ShopApp.Core
         public string? Description { get; set; }
         public decimal Price { get; set; }
         public string? ImageUrl { get; set; }
-        public string? CategoryId { get; set; }
+        public string CategoryId { get; set; }
         public Category? Category { get; set; }
     }
 }
